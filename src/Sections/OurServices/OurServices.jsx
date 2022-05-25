0f4d3bce3ee295b262e42566e38af52cd0030157
LuisEncastin <luisencastin@gmail.com>
2022-05-25 18:58:53 -0500

    Se avanza hasta la parte de servicios

--- src/Sections/OurServices/OurServices.jsx
+++ src/Sections/OurServices/OurServices.jsx
@@ -15,8 +15,19 @@ const OurServices = () => {
                 <AdvantageComponent/>
                 <AdvantageComponent/>
             </div>
-            <div className='ourServicesWrapper'>servicios</div>
-            <div className='servicesGrid'>servicios grid</div>
+            <div className='ourServicesWrapper'>
+                <h1>Servicios</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae culpa perferendis optio reiciendis <br/>
+                 saepe repudiandae repellendus natus recusandae illo ducimus.</p> 
+            </div>
+            <div className='servicesGrid'>
+                <div class="service1 service"> service1 </div>
+                <div class="service2 service"> service2 </div>
+                <div class="service3 service"> service3 </div>
+                <div class="service4 service"> service4 </div>
+                <div class="service5 service"> service5 </div>
+                <div class="service6 service"> service6 </div>
+            </div>
         </section>
     </>
   )
