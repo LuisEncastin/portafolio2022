56972f3b351c8be83867eae3627bcbda13bb19b3
José Luis Encastin Flores <luisencastin@MacBook-Air-de-Jose.local>
2022-06-05 23:41:46 -0500

    Se mejora el menu desplegable

--- src/Sections/NavBar/NavBar.jsx
+++ src/Sections/NavBar/NavBar.jsx
@@ -1,4 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+
+// custom hooks
+import usePrevious from '../../Hooks/usePrevious/usePrevious'
 
 // assets
 import logo from '../../assets/images/logos/oie_transparent.png'
@@ -8,10 +11,43 @@ import burguerMenuIcon from '../../assets/icons/burguerMenuIcon.png'
 import './NavBar.css'
 
 const NavBar = () => {
+
+    let navMenuSettings ={ background : '', borderRadius: 0};
+
+    // states
     const [isNavExpanded, setIsNavExpanded] = useState(false);
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+      const onScroll = () => 
+      setOffset(window.pageYOffset);
+      window.removeEventListener('scroll', onScroll);
+      window.addEventListener('scroll', onScroll, { passive: true });
+      return () => window.removeEventListener('scroll', onScroll);
+    }, [offset]);
+
+    const previousOffset = usePrevious(offset);
+
+    if (offset===0) {navMenuSettings.background='var(--bg01)'; navMenuSettings.borderRadius=0} else { navMenuSettings.background = 'var(--bg01)'; navMenuSettings.borderRadius='0 0 0 0'};
+    if (offset===0 && isNavExpanded) {navMenuSettings.background =`var(--gradient04)`; navMenuSettings.borderRadius='0 0 0 0'} ;
+    if (isNavExpanded && (offset !== previousOffset)) {setIsNavExpanded(!isNavExpanded)};
+
+    const ref = useRef()
+    useEffect(() => {
+      const checkIfClickedOutside = e => {
+        if (isNavExpanded && ref.current && !ref.current.contains(e.target)) {
+          setIsNavExpanded(false);
+        }
+      }
+      document.addEventListener("mousedown", checkIfClickedOutside)
+      return () => {
+        document.removeEventListener("mousedown", checkIfClickedOutside)
+      }
+    }, [isNavExpanded])
+
 
     return (
-      <nav className="navigation">
+      <nav className="navigation" ref={ref}>
         <a href="/" className="brand-name">
           <img src={logo} alt="Logotipo" className='logotipo'/>
         </a>
